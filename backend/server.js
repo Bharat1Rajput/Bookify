0c11dcdd6fff02e9dd2982878cf97e63f7f20b6b
Bharat Singh <br29112003@gmail.com>
2025-09-01 23:20:09 +0530

    cors policy updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,24 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:5173",          
+  "https://appointment-app-topaz.vercel.app/" 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/slot',slotRoutes);
